f5957fa5ad5b6432d305ad756a3a0953aeaea7d6
Liisagrant <lisagrant1990@gmail.com>
2022-01-26 13:54:10 +0100

    tasks from class

--- 1-using_a_single_object_to_create_html/js/petDetails.js
+++ 1-using_a_single_object_to_create_html/js/petDetails.js
@@ -7,8 +7,22 @@ const pet = {
 };
 
 // 1.select header h4
-
+const header4 = document.querySelector("h4");
 // 2.select the .age class
+const  age = document.querySelector(".age");
 // 3.select .friendly class
+const friendlyWrapper = document.querySelector(".friendly");
 
 // 4.change the innerHTML of each of them with data from the object
+header4.innerHTML = pet.type;
+age.innerHTML = pet.age;
+
+//her er det to eks på hvordan bruke true or false i denne settingen.
+/*let friendly;
+if (pet.isFriendly) {
+    friendly = "This pet is friendly";
+}else{
+    friendly ="This pet is not friendly";
+}*/
+friendlyWrapper.innerHTML = pet.isFriendly ? "This pet is friendly" : "This pet is not friendly";
+// some condition ? true : false; dette eks viser hva som logges om tilfelle er true or false.
--- 2-setting_the_innerHTML_of_individual_elements/js/petDetails.js
+++ 2-setting_the_innerHTML_of_individual_elements/js/petDetails.js
@@ -7,6 +7,7 @@ const pet = {
 };
 
 // 1. Select the pet detail dom element by class
+const petDetail = document.querySelector(".pet-detail");
 
 // 3. play with friendly value
 
@@ -15,3 +16,12 @@ const pet = {
 
 // 5. use the colour in a variable
 // 2. change the innerHTML of the pet details
+const myPetColour = pet.colour;
+const myPetType = pet.type;
+const myPetAge = pet.age > 0 ? "super duper" : "no good";
+const myPetIsFriendly = pet.isFriendly;
+
+petDetail.innerHTML = `
+<h4 style="color: ${myPetColour}">Type of pet: ${myPetType}</h4>
+<p>Age: ${myPetAge}</p>
+<p>Friendly:${myPetIsFriendly ? "Yes, super happy and lovely" : "No, angry little pig"}</p>`
