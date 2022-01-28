30d6d534bf5f3253986a0862a0344f46133679c6
Liisagrant <lisagrant1990@gmail.com>
2022-01-28 10:54:48 +0100

    tasks from class

--- 2-setting_the_innerHTML_of_individual_elements/js/petDetails.js
+++ 2-setting_the_innerHTML_of_individual_elements/js/petDetails.js
@@ -25,3 +25,37 @@ petDetail.innerHTML = `
 <h4 style="color: ${myPetColour}">Type of pet: ${myPetType}</h4>
 <p>Age: ${myPetAge}</p>
 <p>Friendly:${myPetIsFriendly ? "Yes, super happy and lovely" : "No, angry little pig"}</p>`
+
+
+let message;
+
+function printMessage (message, message2) {
+    console.log("I am the first message: " + message);
+    console.log("I am the second message: " + message2);
+
+}
+printMessage(" hi there", "I am the second argument");
+
+function multiply (num1, num2) {
+
+    const result = num1 * num2;
+
+   return result;
+}
+
+const result = multiply(5,10);
+console.log(result);
+
+function checkPassword (stringToCheck) {
+    const lengthOfString = stringToCheck.length;
+
+    if(lengthOfString >= 10 ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+const passwordIsValid = checkPassword("some string");
+console.log(passwordIsValid);
--- 3-creating-HTML-from-an-array-of-objects/js/petList.js
+++ 3-creating-HTML-from-an-array-of-objects/js/petList.js
@@ -18,7 +18,32 @@ const pets = [
     },
 ];
 // Select the pet-container class
+const container = document.querySelector(".pet-container");
 
 // Create a html variable to hold our htm in it when we use the loop
+
+let html = "";
 // Loop over the array
+for (let i = 0; i < pets.length; i++){
+    console.log(pets[i]);
+
+    let theType = "";
+
+    if (pets[i].type){
+      theType = pets[i].type;
+    } else {
+        theType = "unknown Type"
+    }
+
+
+    html = html + `
+     <div>
+        <h4> ${theType}</h4>
+        <p>${pets[i].age}</p>
+        <p>${pets[i].friendly ? "Yes is friendly" : "No, is not friendly"}</p>
+     </div> 
+ `;
+}
+
+container.innerHTML= html;
 // change the innerHTML
